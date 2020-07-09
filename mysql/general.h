154cbd32bbb9e8a534c3f8c3081b58bfb101b620
Hannr 1024 <694669095@qq.com>
2020-07-09 14:50:58 +0800

    登录注册，聊天和查看聊天记录的接口函数

--- mysql/general.h
+++ mysql/general.h
@@ -3,6 +3,8 @@
 class user
 {
 public:
+	user():ID(0){};
+	user(int id,std::string username,std::string nickname=""):ID(id),username(username),nickname(nickname){};
 	int ID;
 	std::string username;
 	std::string nickname;
@@ -28,6 +30,9 @@ public:
 class chatmessage
 {
 public:
+	chatmessage():senderID(0),receiveID(0),groupID(0) {};
+	chatmessage(int sid, int rid,int gid, std::string date, std::string time, std::string mess)
+		:senderID(sid),receiveID(rid),groupID(gid),date(date),time(time),message(mess) {}
 	int senderID;
 	int receiveID;
 	int groupID;
